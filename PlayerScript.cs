baea3f5579a9d6da6a862f1b34dcbc14498b0f96
M Bethancourt <matt@mouseandthebillionaire.com>
2020-09-24 11:41:41 -0600

    Did a test message

--- PlayerScript.cs
+++ PlayerScript.cs
@@ -17,6 +17,7 @@ public class PlayerScript : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         healthBar.fillAmount = health;
+        // test test test
     }
 
     // Update is called once per frame
